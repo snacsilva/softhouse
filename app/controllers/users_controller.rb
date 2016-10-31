7e14d0390bf45495b51427d5886f41f3525d5544
Samantha Silva <samanthasilva.n@gmail.com>
2016-10-31 18:27:52 -0300

    Modificação do .gitignore.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -61,6 +61,7 @@ class UsersController < ApplicationController
     end
   end
 
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
@@ -71,4 +72,5 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:name, :nickname, :date_birth, :city, :state, :sex, :age)
     end
+
 end
